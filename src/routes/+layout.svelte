6fd905404415439e87d245ea57439071a7df770b
Adrien Fabre <adrien.fabre@dashlane.com>
2024-03-04 22:40:39 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<header>
+    <h1>P5+Svelte template</h1>
+
+    <nav>
+        <a href="{base}/">Home</a> |
+    </nav>
+</header>
+
+<slot />
+
+<style>
+    :global(.svelte-tabs li.svelte-tabs__tab) {
+        background-color: #bfbec6;
+        margin-right: 4px;
+    }
+</style>
