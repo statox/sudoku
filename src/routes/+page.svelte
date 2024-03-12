ccf8a7b958b12d5efd601bdfa25e974e171eeb95
Adrien Fabre <adrien.fabre@dashlane.com>
2024-03-12 21:59:32 +0100

    Use base path in links to fix github page deployments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
+    import { base } from '$app/paths';
 </script>
 
-<p>Play with <a href="/generator">the generator</a>.</p>
-<p>Explore <a href="/tests">the tests page</a>.</p>
+<p>Play with <a href="{base}/generator">the generator</a>.</p>
+<p>Explore <a href="{base}/tests">the tests page</a>.</p>
