38778cc02998b200920c9ad9cb50c46942c6860e
Adrien Fabre <adrien.fabre@dashlane.com>
2024-03-26 22:14:56 +0100

    Extract game generator to its own file

---
+++ src/lib/services/sudoku/generate/game.ts
@@ -0,0 +1,51 @@
+import type { Grid } from '../types';
+import { countSolutions } from '../solve';
+import { generateNewGridWFC } from './wfc';
+
+// Use generateNewGridWFC() and remove enough values to
+// turn the grid into a grid game;
+// The code is not complete. Some references
+// Answer from a dev with a puzzle app https://gamedev.stackexchange.com/a/76170/64458
+// Simple approach https://puzzling.stackexchange.com/a/146
+// 1. Remove a number,
+// 2. Run a solver,
+// 3. Check if the puzzle is still unique,
+// 4. Repeat until solver is unable to find a solution, or solution is not unique.
+// => Need to have a solver which also check if solutions are unique
+export const generateNewGame = (): Grid => {
+    const grid = generateNewGridWFC();
+
+    const cells = [];
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            cells.push({ row, col });
+        }
+    }
+
+    while (cells.length) {
+        const posRandIndex = Math.floor(Math.random() * cells.length);
+        const { row, col } = cells[posRandIndex];
+        cells.splice(posRandIndex, 1);
+        const savedValue = grid[row][col].value;
+        grid[row][col].value = undefined;
+        grid[row][col].notes = [];
+        grid[row][col].fixed = false;
+
+        const solutionsCount = countSolutions(grid);
+        if (solutionsCount === 'unique') {
+            continue;
+        }
+
+        grid[row][col].value = savedValue;
+    }
+
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (grid[row][col].value) {
+                grid[row][col].fixed = true;
+            }
+        }
+    }
+
+    return grid;
+};
--- src/lib/services/sudoku/generate/index.ts
+++ src/lib/services/sudoku/generate/index.ts
@@ -1,2 +1,3 @@
+export * from './game';
 export * from './generators';
 export * from './wfc';
