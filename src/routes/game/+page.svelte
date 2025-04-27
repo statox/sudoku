0e6d0e7f47c54eb5e8964c52a092547c81d17615
statox <me@statox.fr>
2025-04-27 02:01:43 +0200

    lint

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -14,9 +14,9 @@
         deepCopyGrid,
         getAllGridErrors,
         playOneHint,
-        gridIsValid,
+        gridIsValid
     } from '$lib/services/sudoku';
-    import Sudoku from "$lib/components/Sudoku.svelte";
+    import Sudoku from '$lib/components/Sudoku.svelte';
     import GridStatus from '$lib/components/GridStatus.svelte';
     import { HeadIOS } from '$lib/components/HeadIOS';
     import { getAllHints, type Hint } from '$lib/services/sudoku/hints';
@@ -26,26 +26,26 @@
 
     let grid = generateNewGame();
     let hints: Hint[] = [];
-    const history = [deepCopyGrid(grid)]
+    const history = [deepCopyGrid(grid)];
     let autoRefreshNotes = false;
     let displayHints = false;
     let isFr = false;
 
-    const onCellUpdate = (event: CustomEvent<{cell: Cell, update: CellUpdate}>) => {
+    const onCellUpdate = (event: CustomEvent<{ cell: Cell; update: CellUpdate }>) => {
         updateCell(event.detail.cell, event.detail.update);
         refreshGrid();
-    }
-    const onComputeCellNotes = (event: CustomEvent<{position: {row: number, col: number}}>) => {
+    };
+    const onComputeCellNotes = (event: CustomEvent<{ position: { row: number; col: number } }>) => {
         recomputeNotesForCell(grid, event.detail.position);
         refreshGrid();
     };
 
-    const refreshGrid = (params?: {noHistory: true}) => {
+    const refreshGrid = (params?: { noHistory: true }) => {
         if (autoRefreshNotes) {
             recomputeAllNotes(grid);
         }
         if (displayHints) {
-            refreshHints(grid)
+            refreshHints(grid);
         } else {
             hints = [];
         }
@@ -57,30 +57,30 @@
             return;
         }
         history.push(deepCopyGrid(grid));
-    }
+    };
 
     const solve = () => {
         grid = solveGridDFS(grid);
         refreshGrid();
-    }
+    };
 
     const playHint = () => {
-        const {grid: newGrid, hint} = playOneHint(grid);
+        const { grid: newGrid, hint } = playOneHint(grid);
         grid = newGrid;
         refreshGrid();
         if (hint) {
             hints = [hint];
         }
-    }
+    };
 
     const refreshHints = (grid: Grid) => {
         hints = getAllHints(grid);
-    }
+    };
 
     onMount(() => {
         const storedGrid = getGridFromStorage();
         if (storedGrid) {
-            grid = {...storedGrid};
+            grid = { ...storedGrid };
         }
 
         isFr = $page.url.searchParams.has('fr');
@@ -92,10 +92,30 @@
 <div>
     <h4>{isFr ? 'Contrôles de la grille' : 'Grid controls'}</h4>
     <div class="grid-controls">
-        <button on:click={() => {grid = history.pop(); refreshGrid({noHistory: true})}}>{isFr ? 'Annuler' : 'Prev'}</button>
-        <button on:click={() => {grid = generateNewGame(); refreshGrid()}}>{isFr ? 'Nouvelle grille' : 'New Grid'}</button>
-        <button on:click={() => {solve(); refreshGrid()}}>{isFr ? 'Résoudre' : 'Solve grid'}</button>
-        <button on:click={() => {resetGridToInitialFixedState(grid); refreshGrid()}}>{isFr ? 'Recommencer' : 'Reset grid'}</button>
+        <button
+            on:click={() => {
+                grid = history.pop();
+                refreshGrid({ noHistory: true });
+            }}>{isFr ? 'Annuler' : 'Prev'}</button
+        >
+        <button
+            on:click={() => {
+                grid = generateNewGame();
+                refreshGrid();
+            }}>{isFr ? 'Nouvelle grille' : 'New Grid'}</button
+        >
+        <button
+            on:click={() => {
+                solve();
+                refreshGrid();
+            }}>{isFr ? 'Résoudre' : 'Solve grid'}</button
+        >
+        <button
+            on:click={() => {
+                resetGridToInitialFixedState(grid);
+                refreshGrid();
+            }}>{isFr ? 'Recommencer' : 'Reset grid'}</button
+        >
     </div>
 </div>
 
@@ -103,21 +123,44 @@
     <div>
         <h4>Indices</h4>
         <div>
-            <button on:click={() => {playHint()}}>Jouer un indice</button>
+            <button
+                on:click={() => {
+                    playHint();
+                }}>Jouer un indice</button
+            >
         </div>
     </div>
 {:else}
     <div>
         <h4>Notes controls</h4>
         <div class="notes-controls">
-            <button on:click={() => {autoRefreshNotes = !autoRefreshNotes; refreshGrid()}}>
+            <button
+                on:click={() => {
+                    autoRefreshNotes = !autoRefreshNotes;
+                    refreshGrid();
+                }}
+            >
                 {autoRefreshNotes ? 'Disable' : 'Enable'} notes auto-refresh
             </button>
             {#if !autoRefreshNotes}
-                <button on:click={() => {removeAllNotes(grid); refreshGrid()}}>Remove notes</button>
+                <button
+                    on:click={() => {
+                        removeAllNotes(grid);
+                        refreshGrid();
+                    }}>Remove notes</button
+                >
             {/if}
-            <button on:click={() => {displayHints = !displayHints; refreshGrid()}}>{displayHints ? 'Hide' : 'Display'} hints</button>
-            <button on:click={() => {playHint()}}>Get hint</button>
+            <button
+                on:click={() => {
+                    displayHints = !displayHints;
+                    refreshGrid();
+                }}>{displayHints ? 'Hide' : 'Display'} hints</button
+            >
+            <button
+                on:click={() => {
+                    playHint();
+                }}>Get hint</button
+            >
         </div>
     </div>
 {/if}
@@ -126,24 +169,29 @@
     <h4>{isFr ? 'Surligner' : 'Highlight controls'}</h4>
     <div class="highlights-controls">
         {#each [1, 2, 3, 4, 5, 6, 7, 8, 9] as value}
-            <button class:selected={$selectedHighlight === value} on:click={() => {$selectedHighlight = ($selectedHighlight === value) ? undefined : value}}>{value}</button>
+            <button
+                class:selected={$selectedHighlight === value}
+                on:click={() => {
+                    $selectedHighlight = $selectedHighlight === value ? undefined : value;
+                }}>{value}</button
+            >
         {/each}
     </div>
 </div>
-<br/>
+<br />
 
 {#key grid}
-<Sudoku
-    on:cellUpdate={onCellUpdate}
-    on:computeCellNotes={onComputeCellNotes}
-    gridErrors={getAllGridErrors(grid)}
-    gridIsValid={gridIsValid(grid)}
-    {hints}
-    {grid}
-/>
-{#if !isFr}
-<GridStatus {grid} />
-{/if}
+    <Sudoku
+        on:cellUpdate={onCellUpdate}
+        on:computeCellNotes={onComputeCellNotes}
+        gridErrors={getAllGridErrors(grid)}
+        gridIsValid={gridIsValid(grid)}
+        {hints}
+        {grid}
+    />
+    {#if !isFr}
+        <GridStatus {grid} />
+    {/if}
 {/key}
 
 <style>
